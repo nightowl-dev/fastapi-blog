0fb01a4d687602ff98625f43cae0c4387a27d6d5
faizinkholiq <nasrunfaizinkholiq@gmail.com>
2021-10-31 20:55:10 +0700

    update get api

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional, Text
+from datetime import datetime
 
 app = FastAPI()
+
+postdb = []
+
+#post model
+class Post(BaseModel):
+    id: int
+    title: str
+    content: Text
+    created_ay: datetime = datetime.now()
+    published: Optional[bool] = False
 
 @app.get("/")
 def read_root():
     return {"home": "Home Page"}
+
+
+@app.get("/blog")
+def get_posts():
+    return postdb
