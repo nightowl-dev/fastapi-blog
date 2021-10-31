cb51bac2d80508a55308153c5c0a907027ff41ad
faizinkholiq <nasrunfaizinkholiq@gmail.com>
2021-10-31 21:07:30 +0700

    update delete & update api

--- app.py
+++ app.py
@@ -32,3 +32,13 @@ def add_post(post:Post):
 def get_post(post_id:int):
     post = post_id - 1
     return postdb[post]
+
+@app.post("/blog/{post_id}")
+def update_post(post_id:int, post:Post):
+    postdb[post_id] = post
+    return {"message": "Post has been updated successfully"}
+
+@app.delete("/blog/{post_id}")
+def delete_post(post_id:int):
+    postdb.pop(post_id - 1)
+    return {"message": "Post has been deleted successfully"}
